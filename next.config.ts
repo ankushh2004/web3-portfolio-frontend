b22fdcd00b66bd1be441c247124ae9859449a7ba
ankushh2004 <asanodiya@ancilar.com>
2026-03-12 13:50:29 +0530

    fix: build issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["viem", "wagmi"],
 };
 
 export default nextConfig;
